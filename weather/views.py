69ccc85b5d0b79a47d0089c75b8c899f8a94bd03
Me <edu.kaelteritter@yandex.ru>
2025-05-19 19:36:46 +0400

    Рефакторинг стилей
    1. Добавлены CSS-стили с анимацией
    2. Корректировка html-шаблона

--- weather/views.py
+++ weather/views.py
@@ -13,7 +13,7 @@ def index(request):
     weather = {}
 
     if location:
-        weather = get_weather(settings.OPEN_WEATHER_API_ID, location, units='metric')
+        weather = get_weather(settings.OPEN_WEATHER_API_ID, location, units='metric', lang='ru')
 
     return render(request, 'index.html', {'location_form': location_form, 'weather': weather})
 
