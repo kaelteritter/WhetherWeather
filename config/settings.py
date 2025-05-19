906c5704b1c639c8ddbe3fa0a109f024c7f43bef
Me <edu.kaelteritter@yandex.ru>
2025-05-19 15:13:10 +0400

    Добавлен контроллер, шаблон и маршрутизация главной страницы
    1. Контроллер главной: заглушка генерация тестового
    шаблона
    2. Шаблоны base.html и index.html созданы с одним полем
    ввода
    3. Маршрутизация проброшена в urls приложения
    weather

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -91,5 +91,6 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
